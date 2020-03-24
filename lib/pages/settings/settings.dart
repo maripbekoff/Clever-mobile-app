e771179e3f65b03d0144d5844fb2b7c4f20bd46f
aalystama <allrighty1809@gmail.com>
2020-03-25 03:32:52 +0600

    new settings screen 25/3/20

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -1,14 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kazpost/db/database_helper.dart';
-import 'package:kazpost/pages/settings/pages/faq_settings.dart';
-import 'package:kazpost/pages/settings/pages/profile.dart';
-import 'package:kazpost/pages/settings/pages/security.dart';
-
-final String faq = './assets/img/faq.png';
-final String lang = './assets/img/lang.png';
-final String lock = './assets/img/lock.png';
-final String user = './assets/img/user.png';
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key key}) : super(key: key);
@@ -26,214 +17,196 @@ class _SettingsPageState extends State<SettingsPage> {
     super.initState();
   }
 
-  final int txtColor = 0xFF8465B7;
+  @override
+  Widget build(BuildContext context) {
+    final double _sizedBox = 25;
+
+    return Scaffold(
+      backgroundColor: Color(0xFFFFFFF),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 10,
+        ),
+        children: <Widget>[
+          Text(
+            "Настройки",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+            ),
+          ),
+          SizedBox(height: _sizedBox),
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                databaseHelper.avatar,
+                height: MediaQuery.of(context).size.height / 8,
+              ),
+            ),
+          ),
+          SizedBox(height: _sizedBox / 2),
+          Center(
+            child: Text(
+              databaseHelper.name,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: _sizedBox / 2),
+          AccountView(),
+          SizedBox(height: _sizedBox / 2),
+          AdditionalView(),
+        ],
+      ),
+    );
+  }
+}
+
+class AccountView extends StatelessWidget {
+  const AccountView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFF6F9FF),
-      body: Column(
+    final TextStyle _titleTextStyle = TextStyle(
+      fontSize: 14,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    );
+    final TextStyle _descriptionTextStyle = TextStyle(
+      fontSize: 12,
+      color: Colors.black54,
+    );
+    return Card(
+      color: Colors.white,
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Text(
-              'Настройки',
-              style: TextStyle(fontFamily: 'Montserrat', fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(height: 20),
-          Center(
-            child: Column(
-              children: <Widget>[
-                CircleAvatar(backgroundImage: NetworkImage(databaseHelper.avatar), radius: 40),
-                SizedBox(height: 15),
-                Text(
-                  '${databaseHelper.name}',
-                  style: TextStyle(
-                    fontFamily: "Montserrat",
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 8,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFE4E2F4),
-                    borderRadius: BorderRadius.circular(360),
-                  ),
-                  child: Text(
-                    "${databaseHelper.type}",
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontFamily: "Montserrat",
-                      fontWeight: FontWeight.bold,
-                      color: Color(txtColor),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 35,
-              ),
-              width: double.infinity,
+          ListTile(
+            leading: Container(
+              padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Color(0xFF66BB6A)),
+              child: Icon(
+                Icons.book,
                 color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
-                ),
+                size: 20,
               ),
-              child: ListView(
-                physics: BouncingScrollPhysics(),
-                children: <Widget>[
-                  // Профиль
-                  ListTile(
-                    contentPadding: const EdgeInsets.all(0),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ProfileSettings(),
-                        ),
-                      );
-                    },
-                    leading: Container(
-                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF0157A5),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Image.asset(
-                        user,
-                        height: 25,
-                        width: 25,
-                      ),
-                    ),
-                    title: Text(
-                      "Профиль",
-                      style: TextStyle(fontSize: 17, color: Color(txtColor)),
-                    ),
-                  ),
-                  // Профиль
-
-                  // Безопасность
-                  ListTile(
-                    contentPadding: const EdgeInsets.all(0),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SecuritySettings(),
-                        ),
-                      );
-                    },
-                    leading: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 5,
-                        horizontal: 5,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF0157A5),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Image.asset(
-                        lock,
-                        height: 25,
-                        width: 25,
-                      ),
-                    ),
-                    title: Text(
-                      "Безопасность",
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Color(txtColor),
-                      ),
-                    ),
-                  ),
-                  // Безопасность
-
-                  // Язак приложения
-                  ListTile(
-                    contentPadding: const EdgeInsets.all(0),
-                    onTap: () {},
-                    leading: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 5,
-                        horizontal: 5,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF0157A5),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Image.asset(
-                        lang,
-                        height: 25,
-                        width: 25,
-                      ),
-                    ),
-                    title: Text(
-                      "Язык приложения",
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Color(txtColor),
-                      ),
-                    ),
-                  ),
-                  // Язык приложения
-
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.black12,
-                  ),
-
-                  // FAQ
-                  ListTile(
-                    contentPadding: const EdgeInsets.all(0),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => FaqSettings(),
-                        ),
-                      );
-                    },
-                    leading: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 5,
-                        horizontal: 5,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF0157A5),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Image.asset(
-                        faq,
-                        height: 25,
-                        width: 25,
-                      ),
-                    ),
-                    title: Text(
-                      "Поддержка. FAQ",
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Color(txtColor),
-                      ),
-                    ),
-                  ),
-                ],
+            ),
+            title: Text("Имя и фамилия", style: _titleTextStyle),
+            subtitle:
+                Text("Изменение имени и фамилии", style: _descriptionTextStyle),
+            trailing: IconButton(
+              icon: Icon(Icons.arrow_forward_ios),
+              onPressed: () {},
+            ),
+          ),
+          ListTile(
+            leading: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Color(0xFFFFA726)),
+              child: Icon(
+                Icons
+                    .security, //если Нурали не понравиться дизайн, то использовать verified_user
+                color: Colors.white,
+                size: 20,
               ),
+            ),
+            title: Text("Изменить пароль", style: _titleTextStyle),
+            subtitle:
+                Text("Изменение старого пароля", style: _descriptionTextStyle),
+            trailing: IconButton(
+              icon: Icon(Icons.arrow_forward_ios),
+              onPressed: () {},
+            ),
+          ),
+          ListTile(
+            leading: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Color(0xFFFF7043)),
+              child: Icon(
+                Icons.notifications_active,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+            title: Text("Уведомления", style: _titleTextStyle),
+            subtitle:
+                Text("Настройка уведомлений", style: _descriptionTextStyle),
+            trailing: IconButton(
+              icon: Icon(Icons.arrow_forward_ios),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AdditionalView extends StatelessWidget {
+  const AdditionalView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle _titleTextStyle = TextStyle(
+      fontSize: 14,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    );
+    final TextStyle _descriptionTextStyle = TextStyle(
+      fontSize: 12,
+      color: Colors.black54,
+    );
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          ListTile(
+            leading: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Color(0xFFAB47BC)),
+              child: Icon(
+                Icons.live_help,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+            title: Text("FAQ. Помощь", style: _titleTextStyle),
+            subtitle: Text("Ответы на самые частые вопросы",
+                style: _descriptionTextStyle),
+            trailing: IconButton(
+              icon: Icon(Icons.arrow_forward_ios),
+              onPressed: () {},
+            ),
+          ),
+          ListTile(
+            leading: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Color(0xFFEF5350)),
+              child: Icon(
+                Icons.exit_to_app,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+            title: Text("Выйти из системы", style: _titleTextStyle),
+            trailing: IconButton(
+              icon: Icon(Icons.arrow_forward_ios),
+              onPressed: () {},
             ),
           ),
         ],
